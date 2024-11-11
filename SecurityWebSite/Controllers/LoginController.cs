d7cb768335452f0a3705797727e452bf7fe24734
William <william.harding-1@students.plymouth.ac.uk>
2024-11-11 03:35:24 +0000

    Initial login endpoint using hard coded credentials for testing (will be replaced when database is setup)

---
+++ SecurityWebSite/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SecurityWebSite.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        public struct LoginData
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
+
+        [HttpPost]
+        [Route("/auth/login")]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        public async Task<string> LoginEndpoint([FromBody] LoginData data)
+        {
+
+            if (data.Username == "test" &&  data.Password == "password") 
+            {
+                // WILL RETURN AUTH TOKEN ONCE DATABASE AND JWT SETUP
+                return "Correct Credentials.";
+            }
+
+            Response.StatusCode = StatusCodes.Status401Unauthorized;
+            return "Incorrect Credentials.";
+
+        }
+
+    }
+}
