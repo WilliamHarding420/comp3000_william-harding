3ef0ae2bf4cfdf8cdbd7eb398e5f49b8429d8a05
William <william.harding-1@students.plymouth.ac.uk>
2024-11-11 03:37:07 +0000

    Request utilities for easier POST and GET requests + putting HTML on page from URL

---
+++ SecurityWebSite/Scripts/request_helper.js
@@ -0,0 +1,40 @@
+﻿
+
+function POST_Request(url, jsonData, successCallback, errorCallback) {
+
+    $.ajax({
+        type: "POST",
+        url: url,
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(jsonData),
+        success: successCallback,
+        error: errorCallback
+    })
+
+}
+
+function GET_Request(url, successCallback, errorCallback) {
+
+    $.ajax({
+        type: "GET",
+        url: url,
+        success: successCallback,
+        error: errorCallback
+    })
+
+}
+
+function loadHTML(url, parentElement) {
+
+    GET_Request(url, function (response) {
+
+        parentElement.innerHTML = response;
+
+    }, function (response) {
+
+        alert("Failed to load HTML, URL: " + url);
+        console.log(response);
+
+    });
+
+}
