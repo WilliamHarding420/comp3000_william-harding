9e2c711c90bebf8d333bc5321610d1a33e6df41b
William <william.harding-1@students.plymouth.ac.uk>
2024-11-24 23:03:45 +0000

    Method for making a JWT token for a user

---
+++ SecurityWebSite/JwtUtils.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace SecurityWebSite
+{
+    public class JwtUtils
+    {
+
+        public async static Task<string> AuthorizeUser(int userID)
+        {
+
+            SymmetricSecurityKey securityKey = new(Encoding.UTF8.GetBytes(Program.SECURITY_KEY));
+            SigningCredentials signingCredential = new(securityKey, SecurityAlgorithms.HmacSha256);
+
+            Claim[] tokenClaims =
+            {
+                new Claim("userID", userID.ToString())
+            };
+
+            JwtSecurityToken authToken = new(
+                issuer: "security-website",
+                audience: "user",
+                claims: tokenClaims,
+                expires: DateTime.Now.AddMinutes(15),
+                signingCredentials: signingCredential);
+
+            return new JwtSecurityTokenHandler().WriteToken(authToken);
+
+        }
+
+    }
+}
