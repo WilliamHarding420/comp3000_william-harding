03e45d5508f1f896a053f9fa83c40d688c68e4a9
William <william.harding-1@students.plymouth.ac.uk>
2024-11-24 23:04:07 +0000

    Security utility class for generating a salt and hashing a password

---
+++ SecurityWebSite/SecurityUtils.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace SecurityWebSite
+{
+    public class SecurityUtils
+    {
+
+        public static string GenerateSalt(int length = 10)
+        {
+
+            // Creating byte array for salt and filling it with random bytes
+            byte[] saltBytes = new byte[length * sizeof(char)];
+            RandomNumberGenerator.Fill(saltBytes);
+
+            // Converting the salt bytes to a hex string
+            string saltString = Convert.ToHexString(saltBytes);
+
+            return saltString;
+
+        }
+
+        public static string HashPassword(string password, string salt)
+        {
+
+            string saltedPassword = password + salt;
+
+            byte[] hashBytes = SHA256.HashData(Encoding.UTF8.GetBytes(saltedPassword));
+            string hashHexString = Convert.ToHexString(hashBytes);
+
+            return hashHexString;
+
+        }
+
+    }
+}
