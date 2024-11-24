ddfc3c3ea870632da42fc025be5971b45d4516c8
William <william.harding-1@students.plymouth.ac.uk>
2024-11-24 23:02:33 +0000

    User model for database

---
+++ SecurityWebSite/DatabaseModels/User.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace SecurityWebSite.DatabaseModels
+{
+    [PrimaryKey("UserID")]
+    public class User
+    {
+
+        [Required]
+        public int UserID { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        [Required]
+        public string Salt { get; set; }
+
+    }
+}
