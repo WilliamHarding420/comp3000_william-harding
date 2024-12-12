30e49a2dfbcaddaac2c003b24803a221d099ff06
William <william.harding-1@students.plymouth.ac.uk>
2024-12-12 06:41:48 +0000

    Auth endpoint for MediaMTX http auth, currently always authorized

---
+++ SecurityWebSite/Controllers/RTSPAuth.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SecurityWebSite.Controllers
+{
+
+    [Controller]
+    public class RTSPAuth : Controller
+    {
+
+        public struct AuthDetails
+        {
+            public string User { get; set; }
+            public string Password { get; set; }
+            public string? IP { get; set; }
+            public string? Action { get; set; }
+            public string? Path { get; set; }
+            public string? Protocol { get; set; }
+            public string? ID { get; set; }
+            public string? Query { get; set; }
+
+        }
+
+        [HttpPost]
+        [Route("/auth/rtsp")]
+        public Task RTSPAuthEndpoint([FromBody] AuthDetails details)
+        {
+
+            Response.StatusCode = 200;
+            return Task.CompletedTask;
+
+        }
+
+    }
+}
