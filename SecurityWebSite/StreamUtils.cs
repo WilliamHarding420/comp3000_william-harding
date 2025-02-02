256091bd7f011aad3bbd717fe6b5489de62d0f98
William <william.harding-1@students.plymouth.ac.uk>
2025-02-02 21:08:24 +0000

    Initial FFMPEG usage for publishing stream and getting thumbnail from stream

---
+++ SecurityWebSite/StreamUtils.cs
@@ -0,0 +1,49 @@
+﻿using SecurityWebSite.DatabaseModels;
+using System.Diagnostics;
+
+namespace SecurityWebSite
+{
+    public class StreamUtils
+    {
+
+        public static Task PublishCamera(Camera camera)
+        {
+
+            Process process = new Process();
+
+            process.StartInfo.FileName = "ffmpeg";
+            process.StartInfo.Arguments = $"-i rtsp://{camera.IP}:{camera.Port}/{camera.StreamURL} " +
+                                          $"-f rtsp -rtsp_transport tcp rtsp://admin:admin@mediamtx:8554/{camera.PublishURL}";
+
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+
+            process.Start();
+
+            return Task.CompletedTask;
+
+        }
+
+        public static Task ReadThumbnailFromStream(Camera camera)
+        {
+
+            Process process = new Process();
+
+            process.StartInfo.FileName = "ffmpeg";
+            process.StartInfo.Arguments = $"-y -rtsp_transport tcp -i rtsp://mediamtx:8554/mystream -update true -vframes 1 /app/Thumbnails/{camera.Name}.png";
+
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+
+            process.Start();
+
+            return Task.CompletedTask;
+
+        }
+
+    }
+}
