7c03aff5a12a21db27f61aa9d28dd527fa083a70
William <william.harding-1@students.plymouth.ac.uk>
2025-02-02 00:24:51 +0000

    JS for sending request to endpoint for adding camera from form

---
+++ SecurityWebSite/Scripts/add_camera.js
@@ -0,0 +1,29 @@
+﻿
+document.getElementById("addCamButton").onclick = function () {
+
+    let camName = document.getElementById("camNameInput").value;
+    let camIP = document.getElementById("camIPInput").value;
+    let camPort = document.getElementById("camPortInput").value;
+    let camPublish = document.getElementById("camPublishInput").value;
+
+    let formData = {
+        name: camName,
+        ip: camIP,
+        port: camPort,
+        publishurl: camPublish,
+    }
+
+    POST_Request("/cctv/new", formData, function (response) {
+
+        alert("Camera " + camName + " added.");
+        loadHTMLToID('/pages/cctv.html', 'module-content', 'cctv.js');
+
+    }, function (xhr, response) {
+
+        let json = JSON.parse(xhr["responseJSON"]);
+
+        alert("Error: " + json["error"]);
+
+    });
+
+};
