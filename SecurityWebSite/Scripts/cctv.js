fa18d1f7765a5d469d3f131fc2904ef85e66ec0a
William <william.harding-1@students.plymouth.ac.uk>
2025-02-02 00:24:13 +0000

    Stopped HLS.js from requesting stream data after leaving page, and used new get cameras endpoint for displayinv cameras

--- SecurityWebSite/Scripts/cctv.js
+++ SecurityWebSite/Scripts/cctv.js
@@ -9,12 +9,23 @@ function showStream(streamURL) {
             let hls = new Hls();
             hls.loadSource("http://localhost/stream/" + streamURL + "/index.m3u8");
             hls.attachMedia(player);
+
+            // Event for stopping requesting hls stream after loading another page
+            document.addEventListener("html_load", function () { stopLoadingStreamCallback(hls); });
+
         }
         else if (player.canPlayType('application/vnd.apple.mpegurl')) {
-            player.src = "http://localhost:8888/mystream/index.m3u8";
+            player.src = "http://localhost:8888/" + streamURL + "/index.m3u8";
         }
 
     });
+
+}
+
+function stopLoadingStreamCallback(hls) {
+
+    hls.stopLoad();
+    document.removeEventListener("html_load", function () { stopLoadingStreamCallback(hls); });
 
 }
 
@@ -22,7 +33,7 @@ function showCameras() {
 
     let camera_list = document.getElementById("camera-list");
 
-    GET_Request("/rtsp/v3/paths/list", function (response) {
+    /*GET_Request("/rtsp/v3/paths/list", function (response) {
 
         response["items"].forEach((element) => {
 
@@ -30,6 +41,24 @@ function showCameras() {
             streamButton.innerHTML = element["name"];
             streamButton.onclick = function () {
                 showStream(element["name"]);
+            };
+
+            camera_list.appendChild(streamButton);
+
+        });
+
+    }, null);*/
+
+    GET_Request("/cctv/get/0", function (response) {
+
+        let cameras = JSON.parse(response);
+        console.log(cameras);
+        cameras["cameras"].forEach((element) => {
+
+            let streamButton = document.createElement("button");
+            streamButton.innerHTML = element["Name"];
+            streamButton.onclick = function () {
+                showStream(element["PublishURL"]);
             };
 
             camera_list.appendChild(streamButton);
