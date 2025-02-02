32c6c106cf7d60821fbddc7d03a08cdfa60acae6
William <william.harding-1@students.plymouth.ac.uk>
2025-02-02 21:10:30 +0000

    Endpoints for getting and updating camera thumbnails

---
+++ SecurityWebSite/Controllers/GetThumbnailController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SecurityWebSite.DatabaseModels;
+
+namespace SecurityWebSite.Controllers
+{
+
+    [ApiController]
+    public class GetThumbnailController : Controller
+    {
+
+        [HttpGet]
+        [Route("/thumbnails/{Thumbnail}")]
+        public async Task<ActionResult> GetThumbnail([FromRoute] string Thumbnail)
+        {
+
+            return base.PhysicalFile($"/app/Thumbnails/{Thumbnail}", "image/png");
+
+        }
+
+    }
+}
---
+++ SecurityWebSite/Controllers/UpdateThumbnailEndpoint.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SecurityWebSite.DatabaseModels;
+
+namespace SecurityWebSite.Controllers
+{
+
+    [ApiController]
+    public class UpdateThumbnailEndpoint : Controller
+    {
+
+        [HttpPost]
+        [Route("/cctv/thumbnail/update/{camera}")]
+        public async Task UpdateThumbnail([FromRoute] string camera)
+        {
+
+            Database db = new();
+
+            Camera? cam = db.Cameras.FirstOrDefault(dbCam => dbCam.Name == camera);
+
+            if (cam == null)
+            {
+                Response.StatusCode = 400;
+                return;
+            }
+
+            await StreamUtils.ReadThumbnailFromStream(cam);
+
+            return;
+
+        }
+
+    }
+}
