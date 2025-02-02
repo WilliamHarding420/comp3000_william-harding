3fe64310f37c7a2bcf054b8b60196aee6df50e0a
William <william.harding-1@students.plymouth.ac.uk>
2025-02-02 00:21:08 +0000

    Utilizing RSTP auth

--- SecurityWebSite/Controllers/RTSPAuth.cs
+++ SecurityWebSite/Controllers/RTSPAuth.cs
@@ -1,16 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SecurityWebSite.DatabaseModels;
 
 namespace SecurityWebSite.Controllers
 {
 
-    [Controller]
+    [ApiController]
     public class RTSPAuth : Controller
     {
 
         public struct AuthDetails
         {
-            public string User { get; set; }
-            public string Password { get; set; }
+            public string user { get; set; }
+            public string password { get; set; }
             public string? IP { get; set; }
             public string? Action { get; set; }
             public string? Path { get; set; }
@@ -25,7 +26,25 @@ namespace SecurityWebSite.Controllers
         public Task RTSPAuthEndpoint([FromBody] AuthDetails details)
         {
 
-            Response.StatusCode = 200;
+            Database db = new();
+
+            User? user = db.Users.Where(dbUser => dbUser.Username == details.user).FirstOrDefault();
+
+            if (user == null)
+            {
+                Response.StatusCode = StatusCodes.Status401Unauthorized;
+                return Task.CompletedTask;
+            }
+
+            string hashedPassword = SecurityUtils.HashPassword(details.password, user.Salt);
+
+            if (hashedPassword != user.PasswordHash || !user.CanStream)
+            {
+                Response.StatusCode = StatusCodes.Status401Unauthorized;
+                return Task.CompletedTask;
+            }
+
+            Response.StatusCode = StatusCodes.Status200OK;
             return Task.CompletedTask;
 
         }
