38295b20c48dd492f59d19df8cb2db750fd19007
William <william.harding-1@students.plymouth.ac.uk>
2025-04-23 22:35:55 +0100

    Logic for creation and deletion of locations

---
+++ SecurityWebSite/Controllers/DeleteLocationController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SecurityWebSite.DatabaseModels;
+
+namespace SecurityWebSite.Controllers
+{
+
+    [Route("/cctv/location/remove/{id:int}")]
+    public class DeleteLocationController : Controller
+    {
+
+        [Authorize]
+        [HttpPost]
+        [Produces("application/json")]
+        [Consumes("application/json")]
+        public async Task DeleteLocation([FromRoute] int id)
+        {
+
+            Database db = new Database();
+
+            Location? deleteLocation = db.Locations.FirstOrDefault(loc => loc.LocationID == id);
+
+            if (deleteLocation == null)
+            {
+                Response.StatusCode = 400;
+                return;
+            }
+
+            db.Locations.Remove(deleteLocation);
+            await db.SaveChangesAsync();
+
+            return;
+
+        }
+
+    }
+}
---
+++ SecurityWebSite/Controllers/NewLocationController.cs
@@ -0,0 +1,49 @@
+﻿
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SecurityWebSite.DatabaseModels;
+
+namespace SecurityWebSite.Controllers
+{
+
+    [ApiController]
+    [Route("/cctv/location/new")]
+    public class NewLocationController : Controller
+    {
+
+        public struct LocationData
+        {
+            public string Name { get; set; }
+            public int ParentID { get; set; }
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        public async Task<string> CreateNewLocation([FromBody] LocationData data)
+        {
+
+            if (data.Name.Trim() == "")
+            {
+                Response.StatusCode = 400;
+                return await JsonResponse<string>.ErrorResponse("Location name required.");
+            }
+
+            Database db = new();
+
+            Location location = new Location()
+            {
+                LocationName = data.Name,
+                ParentID = data.ParentID
+            };
+
+            await db.Locations.AddAsync(location);
+            await db.SaveChangesAsync();
+
+            return await JsonResponse<string>.SingleResponse("message", "Location successfully added.");
+
+        }
+
+    }
+}
