8a266033b4510a747580157fdd62e94252d4ffbf
William <william.harding-1@students.plymouth.ac.uk>
2025-04-23 22:35:32 +0100

    Logic for deleting a camera backend

---
+++ SecurityWebSite/Controllers/DeleteCameraController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SecurityWebSite.DatabaseModels;
+
+namespace SecurityWebSite.Controllers
+{
+
+    [Route("/cctv/remove/{id:int}")]
+    public class DeleteCameraController : Controller
+    {
+
+        [HttpPost]
+        [Authorize]
+        [Produces("application/json")]
+        [Consumes("application/json")]
+        public async Task RemoveCamera([FromRoute] int id)
+        {
+
+            Database db = new();
+
+            Camera? camera = db.Cameras.FirstOrDefault(cam => cam.CameraID == id);
+
+            if (camera == null)
+            {
+                Response.StatusCode = 400;
+                return;
+            }
+
+            db.Cameras.Remove(camera);
+            await db.SaveChangesAsync();
+
+            return;
+
+        }
+
+    }
+}
